52b459d892f2743ab9f4f0e1df4386cba9d0af5f
SquirrelRat <37273964+SquirrelRat@users.noreply.github.com>
2026-04-13 10:51:08 -0500

    Add files via upload

--- QuickPortal.cs
+++ QuickPortal.cs
@@ -858,7 +858,7 @@ namespace QuickPortal
                 }
             }
 
-            var sliderButtonWidth = lineLength + 20f;
+            var sliderButtonWidth = totalWidth;
             ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, new Vector2(0f, height + 2f));
             ImGui.PushStyleColor(ImGuiCol.Button, 0);
             ImGui.PushStyleColor(ImGuiCol.ButtonHovered, 0);
